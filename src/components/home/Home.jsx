d6033cbe620c2a576927bf226c046a612aa4d1cc
skylar siggard <siggardskylar@gmail.com>
2019-10-29 11:00:35 -0600

    added home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,9 @@
 import React, {Component} from 'react'
 import './home.scss'
+import Active from './ActiveGames/ActiveGames'
+import Friends from './FriendsList/FriendsList'
+import Search from './Search/Search'
+import HowTo from '../HowTo/HowTo'
 
 export default class Home extends Component {
     constructor() {
@@ -11,7 +15,10 @@ export default class Home extends Component {
     render() {
         return(
             <div className='Home'>
-
+                <Friends/>
+                <Search/>
+                <Active/>
+                <HowTo/>
         </div>
         )
     }
