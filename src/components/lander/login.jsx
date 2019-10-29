0d328cc8b30077f99df0c41ae8c6021608938ee1
Robert Paulson <rjpaulson3@gmail.com>
2019-10-29 10:07:07 -0600

    added redux and put it into Login

--- src/components/lander/login.jsx
+++ src/components/lander/login.jsx
@@ -1,19 +1,24 @@
-import React, {Component} from './node_modules/react'
-import './login.scss'
+import React, { Component } from "./node_modules/react";
+import "./login.scss";
+import { connect } from "react-redux";
+import { updateUser } from "../../redux/reducer";
 
-export default class Login extends Component {
-    constructor() {
-        super()
-        this.state = {
-
-        }
-    }
-    render() {
-        return(
-            <div className='Login'>
-
-        </div>
-        )
-    }
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div className="Login"></div>;
+  }
 }
 
+function mapStateToProps(state){
+    const {user} = state;
+    return {user}
+}
+
+export default connect(
+  null,
+  { updateUser }
+)(Login);
