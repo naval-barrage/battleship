f042c47efeacc70f3a10314ec2bf75612f29b9ea
Spencer Rhoton <rhotonspencer@gmail.com>
2019-10-30 11:31:53 -0600

    set up grids in state

--- src/components/gameroom/EmyGrid/EmyGrid.jsx
+++ src/components/gameroom/EmyGrid/EmyGrid.jsx
@@ -5,10 +5,15 @@ export default class EmyGrid extends Component {
     constructor() {
         super()
         this.state = {
-             // 0 = empty
-            // 1 = occupied
-            // 2 = miss
-            // 3 = hit
+            // 0 = empty
+            // 1 = submarine(3)
+            // 2 = destroyer(2)
+            // 3 = cruiser(3)
+            // 4 = battleship(4)
+            // 5 = carrier(5)
+            // 6 = miss
+            // 7 = hit
+            
             enemyGrid = [
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
