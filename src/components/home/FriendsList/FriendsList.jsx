e9d1cbbf79e9d734a138a8cdf0ab8660bf6f4dc6
skylar siggard <siggardskylar@gmail.com>
2019-10-31 15:18:56 -0600

    added if friends are online

--- src/components/home/FriendsList/FriendsList.jsx
+++ src/components/home/FriendsList/FriendsList.jsx
@@ -1,17 +1,47 @@
 import React, {Component} from 'react'
 import './friendsList.scss'
+import axios from 'axios'
+// import swal from 'sweetalert2'
 
 export default class FriendsList extends Component {
     constructor() {
         super()
         this.state = {
-
+            friendsList: []
         }
     }
+    componentDidMount() {
+        axios.get(`/api/friends`).then(res => {
+            this.setState({
+                friendsList: res.data
+            })
+        })
+    }
+    handleStartGame() {
+        
+    }
+    
     render() {
         return(
             <div className='FriendsList'>
-                Friends
+                {
+                    this.state.friendsList.length ? (
+                        this.state.friendsList.map((friendsList, i) => {
+                            return (
+                                <div className='List-of-friends'>
+                        <div className="FriendOnline">
+                            {`${this.state.friendsList[i].friend_info[0].username}`}
+                            {!this.state.friendsList[i].friendship_info.game_active ? (
+                                <button onClick={() => this.handleStartGame(friendsList.user_id)}>Start Game</button>
+                            ) : ( 
+                                <button>Game Active</button>
+                            )}
+                        </div>
+                    </div>
+                    )
+                })
+                ) : null
+            }
         </div>
         )
     }
