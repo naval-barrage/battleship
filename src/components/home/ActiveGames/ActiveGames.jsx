07a58497e6e304c95dd430d2b617ad2b6a64b766
skylar siggard <siggardskylar@gmail.com>
2019-11-01 16:15:01 -0600

    home needs style but function is there

--- src/components/home/ActiveGames/ActiveGames.jsx
+++ src/components/home/ActiveGames/ActiveGames.jsx
@@ -40,13 +40,22 @@ class ActiveGames extends Component {
                         return (
                             <div className='results-active'>
                                 <div className="ListOfActive">
-                                    {/* {console.log(activeGames.game_info.gameroom_id)} */}
+                                    {/* {console.log(activeGames)} */}
                                     <img src={activeGames.friend_info[0].img} alt='Boat to show ranking'/>
                                     {`Game with ${activeGames.friend_info[0].username} `}
                                 {activeGames.game_info.turn !== activeGames.friend_info[0].user_id ? (
                                     <button onClick={() => this.goToGame(activeGames.game_info.gameroom_id)}>Its your turn!!!!</button>
                                     ) : (
-                                    <button>its not your turn</button>
+                                    <div class="razar">
+                                        <div class="ringbase ring1"></div>
+                                        <div class="ringbase ring2"></div>
+                                        <div class="pulse"></div>
+                                        <div class="pointer">
+                                        <div></div>
+                                        </div>
+                                        <div class="dot pos1"></div>
+                                        <div class="dot pos2"></div>
+                                    </div>
                                 )}
                                 </div>
                             </div>
