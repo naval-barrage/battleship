baf57b2002056e9d4d78705e98c3c9eda54b5967
skylar siggard <siggardskylar@gmail.com>
2019-11-05 12:26:31 -0700

    radar works but lost dots

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -8,10 +8,16 @@ function Alert() {
         <div>
             <div className='not-u-turn'>
             <p>Its Not your turn anymore, please wait for your opponent to make their move.</p>
-            <div class="radar">
-            <div class="pointer"></div>
-            <div class="shadow"></div>
-            </div></div></div>
+            <div className="radar">
+            <div className="pointer"></div>
+            <div className="shadow"></div>
+            </div></div>
+            <div className="dot1 pos11"></div>
+            <div className="dot1 pos21"></div>
+            <div className="dot1 pos31"></div>
+            <div className="dot1 pos41"></div>
+            <div className="dot1 pos51"></div>
+            </div>
     </div>
   );
 }
