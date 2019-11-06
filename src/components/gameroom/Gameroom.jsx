44288e738e1ed560d94a99b760978f45b7ed2099
Robert Paulson <rjpaulson3@gmail.com>
2019-11-06 13:48:26 -0700

    fixed my stupid mistake

--- src/components/gameroom/Gameroom.jsx
+++ src/components/gameroom/Gameroom.jsx
@@ -48,6 +48,11 @@ class Gameroom extends Component {
         })
     }
     
+    changeTurn = ()=>{
+        this.setState({
+            yourTurn: false
+        })
+    }
 
     getGameInfo() {
         axios.get(`/api/game/${+this.props.match.params.gameroom_id}`).then(res => {
